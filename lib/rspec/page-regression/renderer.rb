22b3fed4f19928c2b8625d5e4a7f2ead9b281eee
jekuno <jekuno@users.noreply.github.com>
2016-08-12 10:51:19 +0200

    Disable resize (ignoring PageRegression.page_size option)

--- lib/rspec/page-regression/renderer.rb
+++ lib/rspec/page-regression/renderer.rb
@@ -5,11 +5,11 @@ module RSpec::PageRegression
 
       test_screenshot_path.dirname.mkpath unless test_screenshot_path.dirname.exist?
       # Capybara doesn't implement resize in API
-      unless page.driver.respond_to? :resize
-        page.driver.browser.manage.window.resize_to *RSpec::PageRegression.page_size
-      else
-        page.driver.resize *RSpec::PageRegression.page_size
-      end
+      #unless page.driver.respond_to? :resize
+      #  page.driver.browser.manage.window.resize_to *RSpec::PageRegression.page_size
+      #else
+      #  page.driver.resize *RSpec::PageRegression.page_size
+      #end
       page.driver.save_screenshot test_screenshot_path, :full => true
     end
   end
